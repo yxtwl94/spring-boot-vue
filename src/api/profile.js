423072ebc61f00ceba8d5aa2cc80ed936aa840c8
yxtwl94 <yxtwl95@hotmail.com>
2022-03-23 21:12:03 +0800

    update

--- src/api/profile.js
+++ src/api/profile.js
@@ -7,3 +7,10 @@ export function editUser(data){
         data
     })
 }
+
+export function getUserNum(){
+    return request({
+        url: '/userNum', // url在封装的时候指定了base url: 'api'
+        method: 'get'
+    })
+}
--- src/components/SubComp.vue
+++ src/components/SubComp.vue
@@ -1,12 +1,42 @@
 <template>
   <div>
-    <h1>Subsubsub</h1>
+    <el-table :data="tableData"
+              width="100%"
+              border stripe>
+      <el-table-column
+          prop="id"
+          label="id">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="名字">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          label="操作"
+          width="100">
+        <template v-slot="scope">
+          <el-button @click="handleShowClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button @click="handleDelClick(scope.row)" type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SubComp"
+  name: "SubComp",
+  methods:{
+    getTable(){
+    },
+    handleShowClick(param){
+      console.log(param)
+    },
+    handleDelClick(param){
+      console.log(param)
+    }
+  }
 }
 </script>
 
