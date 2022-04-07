30b47f43f060005d101a804fc768d1d7ceef5b36
oleyang <oleyang@tencent.com>
2022-04-07 12:37:36 +0800

    视频

--- src/components/LiveComp.vue
+++ src/components/LiveComp.vue
@@ -1,16 +1,50 @@
 <template>
-  <div>
-    直播
+  <div style="display: flex;align-items: center;justify-content: center;">
+<!--    居中显示-->
+    <video-player ref="videoPlayer"
+                  :options="playerOptions"
+                  :playsinline="true"
+                  @play="onPlay">
+
+    </video-player>
   </div>
 
 </template>
 
 <script>
+// require styles
+import 'video.js/dist/video-js.css'
+import { videoPlayer } from 'vue-video-player/src'
+
 export default {
-  name: "LiveComp"
+  name: "LiveComp",
+  components: {
+    videoPlayer
+  },
+  methods: {
+    onPlay() {
+      console.log('play');
+    }
+  },
+  data() {
+    return {
+      playerOptions: {
+        height: '360',
+        autoplay: false,
+        muted: true,
+        controls: true,
+        language: 'zh-CN',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{
+          type: "video/mp4",
+          src: "http://vjs.zencdn.net/v/oceans.mp4",
+        }],
+        poster: "http://vjs.zencdn.net/v/oceans.mp4?vframe/jpg/offset/1",
+      }
+    }
+  },
 }
 </script>
 
 <style scoped>
-
 </style>
